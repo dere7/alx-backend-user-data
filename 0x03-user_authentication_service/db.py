dd329e0920d4cc59bee546d360641b06edd55705
Dereje Desta <ddesta777@gmail.com>
2022-08-02 14:07:09 +0300

    worked on task 0 to 5 of 0x03-user_authentication_service project

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.d")
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Creates a new user and saves it to db"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Gets first row found as filtered by kwargs"""
+        user = self._session.query(User).filter_by(**kwargs).one()
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> User:
+        """Updates a user with user_id"""
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            setattr(user, k, v)
+        self._session.commit()
